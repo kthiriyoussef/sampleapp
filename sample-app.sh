af44a09dfabc44c9923327465f5ecf70a87af135
kthiriyoussef <104700801+kthiriyoussef@users.noreply.github.com>
2023-12-03 12:55:10 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -7,6 +5,8 @@ mkdir tempdir/static
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
+
+export PATH="/usr/bin:$PATH"  # Ensure /usr/bin is in the PATH
 
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
@@ -17,6 +17,6 @@ echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
-docker build -t sampleapp .
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a 
+/usr/bin/docker build -t sampleapp .
+/usr/bin/docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+/usr/bin/docker ps -a
