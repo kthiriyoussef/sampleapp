57cb7c007acf8984327055f231b9604bbf443f7a
kthiriyoussef <104700801+kthiriyoussef@users.noreply.github.com>
2023-12-03 14:06:22 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,4 +1,6 @@
-
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
@@ -6,13 +8,14 @@ cp -r static/* tempdir/static/.
 
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
-sudo /usr/bin/docker build -t sampleapp .
-sudo /usr/bin/docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-sudo /usr/bin/docker ps -a
+/usr/bin/docker build -t sampleapp .
+/usr/bin/docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+/usr/bin/docker ps -a
+
